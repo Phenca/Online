256996355fd992da20fa8ef684aba6f7241308e3
Soran <sorantxr@gmail.com>
2024-12-06 15:31:41 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module online.mvc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens online.mvc to javafx.fxml;
+    exports online.mvc;
+}
