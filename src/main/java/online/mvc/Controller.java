5b52fa5dccc9c0af4be0f7dd9c2755acfcedb208
Soran <sorantxr@gmail.com>
2025-01-13 17:19:28 +0100

    implemented switches between interfaces

--- src/main/java/online/mvc/Controller.java
+++ src/main/java/online/mvc/Controller.java
@@ -1,11 +1,18 @@
 package online.mvc;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import online.mvc.models.BaseModel;
+
+import java.io.IOException;
 
 public class Controller {
     private BaseModel model;
@@ -89,13 +96,29 @@ public class Controller {
     private ImageView battery_2;
 
     @FXML
-    protected void log_to_account() {
-
+    protected void log_to_account(ActionEvent evt) {
+        try {
+            FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("main.fxml"));
+            AnchorPane view = fxmlloader.load();
+            Stage new_stage = (Stage) ((Button  ) evt.getSource()).getScene().getWindow();
+            new_stage.setScene(new Scene(view));
+            new_stage.show();
+        } catch (Exception err) {
+            System.err.println(err.getMessage());
+        }
     }
 
     @FXML
-    protected void switch_to_account_creation() {
-
+    protected void switch_to_account_creation(ActionEvent evt) {
+        try {
+            FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("signin.fxml"));
+            AnchorPane view = fxmlloader.load();
+            Stage new_stage = (Stage) ((Button) evt.getSource()).getScene().getWindow();
+            new_stage.setScene(new Scene(view));
+            new_stage.show();
+        } catch (Exception err) {
+            System.err.println(err.getMessage());
+        }
     }
 
     @FXML
@@ -104,8 +127,29 @@ public class Controller {
     }
 
     @FXML
-    protected void switch_to_account_login() {
+    protected void switch_to_account_login(ActionEvent evt) {
+        try {
+            FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("login.fxml"));
+            AnchorPane view = fxmlloader.load();
+            Stage new_stage = (Stage) ((Button) evt.getSource()).getScene().getWindow();
+            new_stage.setScene(new Scene(view));
+            new_stage.show();
+        } catch (Exception err) {
+            System.err.println(err.getMessage());
+        }
+    }
 
+    @FXML
+    protected void disconnect(ActionEvent evt) {
+        try {
+            FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("login.fxml"));
+            AnchorPane view = fxmlloader.load();
+            Stage new_stage = (Stage) ((Button) evt.getSource()).getScene().getWindow();
+            new_stage.setScene(new Scene(view));
+            new_stage.show();
+        } catch (Exception err) {
+            System.err.println(err.getMessage());
+        }
     }
 
 
